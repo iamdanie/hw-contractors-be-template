5fd0bcd4abb2f42e2547d34f1b96894efc72a2e5
Daniela Bravo <therealdanie@protonmail.com>
2024-03-22 12:58:14 -0600

    2 - GET /contracts implementation

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { sequelize, Profile } = require('./model')
+const { sequelize } = require('./model')
 const { getProfile } = require('./middleware/getProfile')
 const { Op } = require('sequelize')
 const app = express()
@@ -21,6 +21,20 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
   const contract = await Contract.findOne({
     where: {
       [Op.and]: [{ id }, profileCondition],
+    },
+  })
+  if (!contract) return res.status(404).end()
+  res.json(contract)
+})
+
+app.get('/contracts', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models')
+  const { id: profileId, type } = req?.profile ?? {}
+  const profileCondition =
+    type === 'client' ? { ClientId: profileId } : { ContractorId: profileId }
+  const contract = await Contract.findAll({
+    where: {
+      [Op.and]: [profileCondition, { status: { [Op.ne]: 'terminated' } }],
     },
   })
   if (!contract) return res.status(404).end()
