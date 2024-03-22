5425f8684bfb2380a89c404d2c72df6c4b69afef
Daniela Bravo <therealdanie@protonmail.com>
2024-03-22 15:45:19 -0600

    4 - POST /jobs/:id/pay implementation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { sequelize, Job } = require('./model')
+const { sequelize } = require('./model')
 const { getProfile } = require('./middleware/getProfile')
-const { Op, where } = require('sequelize')
+const { Op } = require('sequelize')
 const app = express()
 app.use(bodyParser.json())
 app.set('sequelize', sequelize)
@@ -47,19 +47,93 @@ app.get('/contracts', getProfile, async (req, res) => {
 /**
  *
  * @returns unpaid jobs
+ *
  */
 app.get('/jobs/unpaid', getProfile, async (req, res) => {
-  const { Contract } = req.app.get('models')
+  const { Contract, Job } = req.app.get('models')
   const { id: profileId, type } = req?.profile ?? {}
   const profileCondition =
     type === 'client' ? { ClientId: profileId } : { ContractorId: profileId }
   const jobs = await Job.findAll({
-    include: { model: Contract, where: profileCondition },
+    include: {
+      model: Contract,
+      where: { [Op.and]: [profileCondition, { status: 'in_progress' }] },
+    },
     where: {
       paid: null,
     },
   })
   if (!jobs) return res.status(404).end()
   res.json(jobs)
+})
+module.exports = app
+
+/**
+ * Pays for a given job id the amount within the payload
+ * @returns paid job
+ */
+app.post('/jobs/:id/pay', getProfile, async (req, res) => {
+  try {
+    const result = await sequelize.transaction(async (t) => {
+      const { Contract, Job, Profile } = req.app.get('models')
+      const { id: profileId, type, balance } = req?.profile ?? {}
+      const { id: jobId } = req.params
+      const { amount } = req?.body ?? { amount: 0 }
+
+      if (type === 'contractor') {
+        return { status: 403 }
+      }
+      if (!amount) {
+        return { hasError: true, status: 400 }
+      }
+      const job = await Job.findOne({
+        include: {
+          model: Contract,
+          where: {
+            [Op.and]: [{ ClientId: profileId }, { status: 'in_progress' }],
+          },
+        },
+        where: { id: jobId },
+      })
+
+      if (!job) return { hasError: true, status: 404 }
+
+      if (balance < amount || amount !== job.price) {
+        return { hasError: true, status: 400 }
+      }
+
+      job.paid = true
+      job.paymentDate = new Date()
+      await job.save({ transaction: t })
+
+      await Profile.update(
+        {
+          balance: Number(balance) - amount,
+        },
+        {
+          where: { id: profileId },
+          transaction: t,
+        },
+      )
+
+      const contractor = await Profile.findOne({
+        where: { id: job.Contract.ContractorId },
+      })
+
+      contractor.balance = Number(contractor.balance) + amount
+      await contractor.save({ transaction: t })
+
+      return job
+    })
+
+    if (result.status && result.hasError) {
+      res.status(result.status).end()
+    } else {
+      res.json(result)
+    }
+  } catch (e) {
+    console.error(e, e.message)
+    res.status(500).end()
+  }
 })
 module.exports = app
