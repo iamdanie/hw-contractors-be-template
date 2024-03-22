40481ed68a8072d7f0ba7a22993a2b25aceabb72
Daniela Bravo <therealdanie@protonmail.com>
2024-03-22 12:53:47 -0600

    Solves broken route /contracts/:id issues

--- src/app.js
+++ src/app.js
@@ -1,22 +1,26 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { sequelize } = require('./model')
+const { sequelize, Profile } = require('./model')
 const { getProfile } = require('./middleware/getProfile')
+const { Op } = require('sequelize')
 const app = express()
 app.use(bodyParser.json())
 app.set('sequelize', sequelize)
 app.set('models', sequelize.models)
 
 /**
- * FIX ME!
+ *
  * @returns contract by id
  */
 app.get('/contracts/:id', getProfile, async (req, res) => {
   const { Contract } = req.app.get('models')
+  const { id: profileId, type } = req?.profile ?? {}
   const { id } = req.params
+  const profileCondition =
+    type === 'client' ? { ClientId: profileId } : { ContractorId: profileId }
   const contract = await Contract.findOne({
     where: {
-      id,
+      [Op.and]: [{ id }, profileCondition],
     },
   })
   if (!contract) return res.status(404).end()
