5aaa67b150233a12d77bc39b322686b47536f882
Daniela Bravo <therealdanie@protonmail.com>
2024-03-22 14:18:13 -0600

    3 - GET /jobs/unpaid implementation

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const { sequelize } = require('./model')
+const { sequelize, Job } = require('./model')
 const { getProfile } = require('./middleware/getProfile')
-const { Op } = require('sequelize')
+const { Op, where } = require('sequelize')
 const app = express()
 app.use(bodyParser.json())
 app.set('sequelize', sequelize)
@@ -26,7 +26,10 @@ app.get('/contracts/:id', getProfile, async (req, res) => {
   if (!contract) return res.status(404).end()
   res.json(contract)
 })
-
+/**
+ *
+ * @returns contracts from a corresponding profile
+ */
 app.get('/contracts', getProfile, async (req, res) => {
   const { Contract } = req.app.get('models')
   const { id: profileId, type } = req?.profile ?? {}
@@ -39,5 +42,24 @@ app.get('/contracts', getProfile, async (req, res) => {
   })
   if (!contract) return res.status(404).end()
   res.json(contract)
+})
+
+/**
+ *
+ * @returns unpaid jobs
+ */
+app.get('/jobs/unpaid', getProfile, async (req, res) => {
+  const { Contract } = req.app.get('models')
+  const { id: profileId, type } = req?.profile ?? {}
+  const profileCondition =
+    type === 'client' ? { ClientId: profileId } : { ContractorId: profileId }
+  const jobs = await Job.findAll({
+    include: { model: Contract, where: profileCondition },
+    where: {
+      paid: null,
+    },
+  })
+  if (!jobs) return res.status(404).end()
+  res.json(jobs)
 })
 module.exports = app
